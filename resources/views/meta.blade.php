74423323a46d6bb042fa9a02080e751a8cec0fbc
Sarl james Sebios <ssarljames@hotmail.com>
2020-04-22 00:57:08 +0800

    updates

---
+++ resources/views/meta.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VSU CS3</title>
+
+
+    <meta property="og:url" content="https://vsu-cs3.now.sh/{{ $url }}"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:title" content="{{ $title }}"/>
+    <meta property="og:description" content="{{ $description }}"/>
+    <meta property="og:image" content="https://scontent.fceb1-1.fna.fbcdn.net/v/t1.0-9/67886435_112616973416965_5052278787446669312_n.jpg?_nc_cat=100&amp;_nc_sid=09cbfe&amp;_nc_eui2=AeHLYqt6sP01dC6ar2iw9ykNUmCoPxuomacYRZTQZToYvo5b8kw8QhhhCfis8mrt9UaE_nIv-kS1j7tg7Aq7e8kLaBty6wkH1LRP7cFy58fArg&amp;_nc_ohc=0On_yxUjxtsAX8d8T_F&amp;_nc_ht=scontent.fceb1-1.fna&amp;oh=4bb23f4526b803bbc026e5bd337d5b62&amp;oe=5E963B9B"/>
+
+
+</head>
+<body>
+    Redirecting...
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,32 @@
 */
 
 // use App\Models\ServiceRate;
-use App\Models\StationUsageLog;
-use Carbon\Carbon;
+// use App\Models\StationUsageLog;
+// use Carbon\Carbon;
 
-use App\Models\AccessToken;
+// use App\Models\AccessToken;
+
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
-Route::get('', function(){
+// Route::get('', function(){
 
-    $max = Carbon::parse(date('Y-m-d') . ' ' . StationUsageLog::MAX_TIME_OUT);
+//     $max = Carbon::parse(date('Y-m-d') . ' ' . StationUsageLog::MAX_TIME_OUT);
 
 
-    return $max->lte('2020-03-30 20:26:00') ? '1' : '0';
+//     return $max->lte('2020-03-30 20:26:00') ? '1' : '0';
+// });
+
+
+// Route::get('login', function(){
+//     return 'Test login';
+// })->name('login');
+
+Route::get('share/posts/{post}', function(Post $post){
+
+    $url = "posts/" . $post->id;
+    $title = $post->title;
+    $description = "Posted by " . $post->user->fullname . " at " . $post->created_at->format('M d, Y');
+
+    return view('meta', compact('title', 'description', 'url'));
 });
-
-
-Route::get('login', function(){
-    return 'Test login';
-})->name('login');
